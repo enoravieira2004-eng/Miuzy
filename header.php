12d6c60b724202d840496a3579acb2c32674d30a
insaf11 <insafkarraz11@gmail.com>
2025-12-09 14:48:43 +0100

    creation du menu

--- header.php
+++ header.php
@@ -5,18 +5,18 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/info.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/header.css">
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    
+
 <header class="miuzy-header">
     <div class="miuzy-container">
 
         <!-- Logo SVG à gauche -->
         <div class="logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/miuzy.svg" alt="Miuzy">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_miuzy.svg" alt="Miuzy">
         </div>
 
         <!-- Menu à droite -->
