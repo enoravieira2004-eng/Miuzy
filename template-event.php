151d8acd252fa6d9225dd929b7ec8f2fc8a44735
insaf11 <insafkarraz11@gmail.com>
2025-12-10 16:23:07 +0100

    linked button mon-evenement to event page

--- template-event.php
+++ template-event.php
@@ -9,12 +9,13 @@ get_header();
 <div class="event-page">
 
     <!-- HERO -->
-    <section class="event-hero" style="background-image:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pexels-thibault-trillet-44912-167636.jpg');">
-        <div class="event-hero-overlay"></div>
-        <div class="event-hero-content container">
-            <h1>LÀ OÙ LES<br><span>ÉVÈNEMENTS LOCAUX</span><br>PRENNENT <strong>VIE !</strong></h1>
-        </div>
-    </section>
+ <section class="event-hero" style="background-image:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pexels-thibault-trillet-44912-167636.jpg');">
+    <div class="event-hero-overlay"></div>
+    <div class="event-hero-content container">
+        <h1>LÀ OÙ LES<br><span>ÉVÈNEMENTS LOCAUX</span><br>PRENNENT <strong>VIE !</strong></h1>
+    </div>
+</section>
+
 
     <div class="container event-main py-5">
 
